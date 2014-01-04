f5e58f23228a64c647d50dacc7848b0dac3b41b7
Dan Williams <dawilliams929@gmail.com>
2014-01-03 16:35:14 -0800

    add validations for location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,9 @@
 class Location < ActiveRecord::Base
   has_many :observations
   has_many :users, through: :observations
+  
+  validates :name, :latitude, :longitude, presence: true
+  validates :latitude, numericality: { greater_than: -90, less_than: 90 }
+  validates :longitude, numericality: { greater_than: -180, less_than: 180 }
+  validates :name, length: { maximum: 100, allow_blank: false }, uniqueness: true
 end
