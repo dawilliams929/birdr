27fa0ddb3d61e02a8473299ed1b326e5e20118d1
Dan Williams <dawilliams929@gmail.com>
2014-01-03 16:28:07 -0800

    add validations for observation

--- app/models/observation.rb
+++ app/models/observation.rb
@@ -3,4 +3,16 @@ class Observation < ActiveRecord::Base
   # belongs_to :user
   has_many :sightings
   has_many :species, through: :sightings
+  
+  validates :location_id, :user_id, numericality: { only_integer: true }, presence: true
+  validates :date, presence: true
+  validate :date_cannot_be_in_future
+  
+  private
+  
+  def date_cannot_be_in_future
+    if date > Date.today
+      errors.add(:date, "can't be in the future")
+    end
+  end
 end
