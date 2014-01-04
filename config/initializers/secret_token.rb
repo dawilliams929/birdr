19342d390e5a3f271213d58b7be893b2e1ae5493
Dan Williams <dawilliams929@gmail.com>
2014-01-03 16:01:06 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Birdr::Application.config.secret_key_base = '6fa9d84e7f761df9ebbb6775e367a1e23304c7cfd6438dcb447c4706156be48954be2280c05e7e2f0d5bc276fa6cafeca60c0e257a2fa7925c79fdbf2a1d9787'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Birdr::Application.config.session_store :cookie_store, key: '_birdr_session'
