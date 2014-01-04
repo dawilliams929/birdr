212a276c707626257da5212565decbb89a7544c9
Dan Williams <dawilliams929@gmail.com>
2014-01-03 16:47:09 -0800

    add validations to species

--- app/models/species.rb
+++ app/models/species.rb
@@ -2,7 +2,20 @@ class Species < ActiveRecord::Base
   has_many :sightings
   has_many :observations, through: :sightings
   
+  validates :common_name, :scientific_name, :family, presence: true
+  validates :common_name, uniqueness: true, length: { minimum: 2, maximum: 100 }
+  validates :scientific_name, :uniquenes: true, length: { minimum: 5, maximum: 100 }
+  validate :family_ends_in_idae
+  
   def genus
     scientific_name.split(" ").first
+  end
+  
+  private
+  
+  def family_ends_in_idae
+    unless family.end_with?("idae")
+      errors.add(:family, "family must end with 'idae'")
+    end
   end
 end
