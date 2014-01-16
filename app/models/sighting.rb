e34c8c7c5ca5fb57122e519d4868e494ac326eb9
Dan Williams <dawilliams929@gmail.com>
2014-01-15 19:18:16 -0800

    shit doesn't work

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -7,4 +7,13 @@ class Sighting < ActiveRecord::Base
             presence: true,
             numericality: { only_integer: true } # needed to stop validating :observation_id for some reason
   validates :count, numericality: { greater_than: 0 }
+  
+  def species_name
+    species.try(:common_name)
+  end
+  
+  def species_name=(name)
+    self.species = Species.where(common_name: name)
+  end
+    
 end
