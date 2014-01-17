02ebc5d8036427f3ea1e4ecb4b5b2ef3eb2713c5
Dan Williams <dawilliams929@gmail.com>
2014-01-16 18:18:40 -0800

    autocomplete work with common and scientific name

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -1,6 +1,10 @@
 class SpeciesController < ApplicationController
   def index
-    @species = Species.where("common_name LIKE ?", "%#{params[:term]}%")
+    @species = Species.where(
+      "common_name LIKE ? OR scientific_name LIKE ?",
+      "%#{params[:term]}%",
+      "%#{params[:term]}%"
+    )
     render json: @species.map(&:to_s)
   end
 end
