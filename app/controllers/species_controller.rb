5d741ddced597dd73a9895a9d5a2f3fe762d88cb
Dan Williams <dawilliams929@gmail.com>
2014-01-23 19:12:43 -0800

    change back to LIKE for now, left comment to remind when time to acually deploy

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -1,10 +1,10 @@
 class SpeciesController < ApplicationController
   def index
     @species = Species.where(
-      "common_name ILIKE ? OR scientific_name ILIKE ?",
+      "common_name LIKE ? OR scientific_name LIKE ?",
       "%#{params[:term]}%",
       "%#{params[:term]}%"
-    )
+    ) # change LIKE to ILIKE for postgres for heroku
     render json: @species.map(&:to_s)
   end
 end
