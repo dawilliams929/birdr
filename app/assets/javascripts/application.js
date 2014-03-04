2bafc19e5b1f15065c75bb3ba36b9a9602f57271
Dan Williams <dawilliams929@gmail.com>
2014-03-03 20:45:23 -0800

    clicking on map now creates icon which can be dragged

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,8 +19,11 @@
 //= require_tree .
 
 
-$(document).on('nested:fieldAdded', function (event) {
-  $("input[id$='species_name']").autocomplete({
-    source: $("input[id$='species_name']").data('autocomplete-source')
-  });
+$(document).on('ready', function() {
+	$(document).on('nested:fieldAdded', function (event) {
+	  $("input[id$='species_name']").autocomplete({
+	    source: $("input[id$='species_name']").data('autocomplete-source')
+	  });
+	});
 });
+
