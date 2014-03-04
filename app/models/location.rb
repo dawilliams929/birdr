c5ff2c1b9e3f2e624df015521b3378f7d391843b
Dan Williams <dawilliams929@gmail.com>
2014-03-03 21:59:59 -0800

    edit validations for readability

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,7 +2,16 @@ class Location < ActiveRecord::Base
   has_many :observations
   
   validates :name, :latitude, :longitude, presence: true
-  validates :latitude, numericality: { greater_than: -90, less_than: 90 }
-  validates :longitude, numericality: { greater_than: -180, less_than: 180 }
-  validates :name, length: { maximum: 100, allow_blank: false }, uniqueness: true
+  validates :latitude, numericality: {
+                         greater_than: -90,
+                         less_than: 90
+                       }
+  validates :longitude, numericality: {
+                          greater_than: -180,
+                          less_than: 180
+                        }
+  validates :name,uniqueness: true, length: {
+                                      maximum: 100,
+                                      allow_blank: false
+                                    }
 end
