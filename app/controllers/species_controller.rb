62e2c753c98c469bca4ffc7e8e521678810bcfc2
Dan Williams <dawilliams929@gmail.com>
2014-03-12 22:23:57 -0700

    fixed json format

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -10,20 +10,49 @@ class SpeciesController < ApplicationController
   
   def show
     @species = Species.find(params[:id])
-    
-    sightings = Sighting.where(species_id: @species.id)
+        
+    sightings = @species.sightings
     observations = sightings.map { |sighting| sighting.observation }
-    locations = observations.map { |observation| observation.location }
-    users = observations.map { |observation| observation.user }
+    
+    locations = observations.map(&:location).uniq
+    
+    locations = locations.map do |location|
+      {
+        name: location.name,
+        lat: location.latitude,
+        long: location.longitude,
+        id: location.id
+      }
+    end
+    
+    locations.each do |loc|
+      obs = []
+      observations.each do |observation|
+        if observation.location.id == loc[:id]
+          obs << { 
+            user: observation.user.email,
+            count: 0,
+            date: observation.date.to_date.to_s,
+            observation_id: observation.id
+          }
+        end
+      end
+      loc[:data] = obs
+    end
+    
     
     respond_to do |format|
       format.html
-      format.json { render json: {
-        sightings: sightings,
-        observations: observations,
-        locations: locations,
-        users: users
-      }}
+      # format.json { render json: {
+    #     sightings: sightings,
+    #     observations: observations,
+    #     locations: locations,
+    #     users: users
+    #   }}
+    
+      format.json {
+        render json: locations
+      }
     end
   end
 end
