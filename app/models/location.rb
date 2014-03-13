ac2acff80e77aedbf78b25df75ec688005243516
Dan Williams <dawilliams929@gmail.com>
2014-03-13 11:32:06 -0700

    location#show now shows list of species seen there + num of species

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,15 +3,22 @@ class Location < ActiveRecord::Base
   
   validates :name, :latitude, :longitude, presence: true
   validates :latitude, numericality: {
-                         greater_than: -90,
-                         less_than: 90
-                       }
+                                       greater_than: -90,
+                                       less_than: 90
+                                     }
   validates :longitude, numericality: {
-                          greater_than: -180,
-                          less_than: 180
-                        }
-  validates :name,uniqueness: true, length: {
-                                      maximum: 100,
-                                      allow_blank: false
-                                    }
+                                        greater_than: -180,
+                                        less_than: 180
+                                      }
+  validates :name, uniqueness: true, length: { maximum: 100, allow_blank: false }
+  
+  def species
+    species = []
+    self.observations.each { |observation| species += observation.species }
+    species.uniq.sort_by! { |sp| sp.id }
+  end
+  
+  def num_of_species
+    self.species.count
+  end                                
 end
