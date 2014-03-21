75f0265cf7160c93c792503f8286e03cb91afb14
Dan Williams <dawilliams929@gmail.com>
2014-03-21 16:32:33 -0700

    refactored a bit

--- app/assets/javascripts/showSpecies.js
+++ app/assets/javascripts/showSpecies.js
@@ -8,21 +8,24 @@ function initialize() {
 	map.fitBounds(bounds);
 }
 
+function createMarker(location, map) {
+	var lat = location.lat;
+	var lng = location.long;
+	var latLng = new google.maps.LatLng(lat, lng);
+	
+	return new google.maps.Marker({
+		position: latLng,
+		map: map
+	});
+}
+
 function setMarkers(map, bounds) {
 	var infoWindow = new google.maps.InfoWindow();
 	var jsonUrl = "/species/" + id + ".json";
 	
 	$.getJSON(jsonUrl, function (locations) {
 		$.each(locations, function (index, location) {
-			var lat = location.lat;
-			var lng = location.long;
-			var latLng = new google.maps.LatLng(lat, lng);
-			
-			var marker = new google.maps.Marker({
-				position: latLng,
-				map: map
-			});
-			
+			var marker = createMarker(location, map);
 			bounds.extend(marker.position);
 			
 			google.maps.event.addListener(marker, 'click', function () {
@@ -30,7 +33,7 @@ function setMarkers(map, bounds) {
 				$.each(location.data, function (index, entry) {
 					rowDataString += "<tr><td>" + entry.date + "</td><td>" + entry.count + "</td><td>" + entry.user + "</td><td><a href='/observations/" + entry.observation_id + "'>checklist</a></td></tr>";
 				});
-				var contentString = "<h3>" + location.name + "</h3><table><tr><th>DATE</th><th>#</th><th>OBSERVER</th></tr>" + rowDataString + "</table>";
+				var contentString = "<h3>" + location.name + "</h3><table border='1'><tr><th>DATE</th><th>#</th><th>OBSERVER</th></tr>" + rowDataString + "</table>";
 				infoWindow.setContent(contentString);
 				infoWindow.open(map, marker);
 			});
