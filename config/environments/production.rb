5d56506127b168704755d0ab77ca4743fe69c6d2
Dan Williams <dawilliams929@gmail.com>
2014-03-26 22:33:33 -0700

    precompile js assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,9 @@ Birdr::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( newLocation.js )
+  config.assets.precompile += %w( showSpecies.js )
+  
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
