43e38f8ff96091a385fa4231a8db4897cc15afff
Dan Williams <dawilliams929@gmail.com>
2014-03-26 21:38:32 -0700

    center map on group of markers

--- app/assets/javascripts/showSpecies.js
+++ app/assets/javascripts/showSpecies.js
@@ -1,13 +1,19 @@
 function initialize() {
 	var mapOptions = {
+		center: new google.maps.LatLng(38, -122),
   	zoom: 9,
     mapTypeId: google.maps.MapTypeId.SATELLITE
 	};
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 	var bounds = new google.maps.LatLngBounds();
-  setMarkers(map, bounds);
-	map.fitBounds(bounds);
-	console.log(bounds.isEmpty());
+	
+  setMarkers(map, bounds, function (markers) {
+		if (markers.length > 0) {
+			map.fitBounds(bounds);
+		}
+  });
+	
+	zoomOutIfTooClose(map);
 }
 
 function createMarker(location, map) {
@@ -21,13 +27,15 @@ function createMarker(location, map) {
 	});
 }
 
-function setMarkers(map, bounds) {
+function setMarkers(map, bounds, callback) {
 	var infoWindow = new google.maps.InfoWindow();
 	var jsonUrl = "/species/" + id + ".json";
+	var markers = [];
 	
 	$.getJSON(jsonUrl, function (locations) {
 		$.each(locations, function (index, location) {
 			var marker = createMarker(location, map);
+			markers.push(marker);
 			
 			bounds.extend(marker.position);
 			
@@ -36,11 +44,21 @@ function setMarkers(map, bounds) {
 				$.each(location.data, function (index, entry) {
 					rowDataString += "<tr><td>" + entry.date + "</td><td>" + entry.count + "</td><td>" + entry.user + "</td><td><a href='/observations/" + entry.observation_id + "'>checklist</a></td></tr>";
 				});
-				var contentString = "<h3>" + location.name + "</h3><table><thead><tr><th>DATE</th><th>#</th><th>OBSERVER</th></tr></thead><tbody>" + rowDataString + "</tbody></table>";
+				var contentString = "<h5>" + location.name + "</h5><table class='table  table-condensed table-striped'><thead><tr><th>Date</th><th>#</th><th>Observer</th></tr></thead><tbody>" + rowDataString + "</tbody></table>";
 				infoWindow.setContent(contentString);
 				infoWindow.open(map, marker);
 			});
 		});
+		
+		callback(markers);
+	});
+}
+
+function zoomOutIfTooClose(map) {
+	google.maps.event.addListenerOnce(map, 'zoom_changed', function () {
+		if (map.getZoom() > 9) {
+			map.setZoom(9);
+		}
 	});
 }
 
